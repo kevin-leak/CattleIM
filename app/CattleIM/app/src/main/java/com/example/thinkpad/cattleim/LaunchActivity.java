ce29c7d98c21fbc976d9491aece7955ea866909b
kevin-leak <956572845@qq.com>
2018-10-02 18:52:54 +0800

    change the launch

---
+++ app/CattleIM/app/src/main/java/com/example/thinkpad/cattleim/LaunchActivity.java
@@ -0,0 +1,14 @@
+package com.example.thinkpad.cattleim;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class LaunchActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+}
